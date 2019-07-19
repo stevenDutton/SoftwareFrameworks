58f069ac47226a734ce7fc241113181b0ff3b83e
stevenDutton <steven.dutton@griffithuni.edu.au>
2019-07-19 13:52:25 +1000

    fixed merge conflict

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-console.log("Steven Dutton - 3813ICT Week 2");
+console.log("Oyyyyyy yeeeeeah mate");
 console.log("hello world");
 
 var addingFunction = (x,y) =>  x+y;
 console.log(addingFunction(5,10));
-=======
-console.log("Oyyyyyy yeeeeeah mate");
-console.log("hello world");
->>>>>>> 5b893bdd1fba56810319c41a91ee39bf07ebb9ff
